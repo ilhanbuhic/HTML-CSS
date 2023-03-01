09eb46dd0044ad06eb26d4f7c0e55ab5acf703fb
ilhanbuhic <ilhanbuhic@hotmail.com>
2023-03-01 20:51:11 +0100

    HTML&CSS

--- Login Form/index.js
+++ Login Form/index.js
@@ -1,1 +1,27 @@
 "use strict"
+
+const placeholder = document.querySelectorAll("input")
+console.log(placeholder)
+
+// This won't work, because document.querySelectorAll returns a nodeList which is an array
+// so we can't assign eventListener to all of them that way
+// placeholder.addEventListener("click", function () {
+//   for (let i = 0; i < placeholder.length; i++) {
+//     placeholder[i].placeholder = ""
+//   }
+// })
+
+// We have to do it like this:
+// 1st way
+for (let i = 0; i < placeholder.length; i++) {
+  placeholder[i].addEventListener("click", function () {
+    placeholder[i].placeholder = ""
+  })
+}
+
+// 2nd way
+// for (const input of placeholder) {
+//   input.addEventListener("click", function () {
+//     input.placeholder = ""
+//   })
+// }
